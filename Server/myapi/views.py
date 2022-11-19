a4c10d97c6134945eae57735460902194dc6cbd3
Shing Him Yip <shing.yip.2020@live.rhul.ac.uk>
2022-11-19 15:24:33 +0000

    upload the get data file

--- Server/myapi/views.py
+++ Server/myapi/views.py
@@ -4,5 +4,7 @@ from django.shortcuts import render
 
 def getRoutes(request):
 
-    return JsonResponse("Tewerwerwerst",safe=False)
+    list = [1,2,3,4,5,6,7,8,10]
+
+    return JsonResponse(list,safe=False)
 
