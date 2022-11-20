68e2c26e0b7b42498cb04e4b2b1686e4e57df13b
Shing Him Yip <shing.yip.2020@live.rhul.ac.uk>
2022-11-20 01:52:40 +0000

    test the post and get function

--- Server/myapi/urls.py
+++ Server/myapi/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('test',view=views.getRoutes,name="routes")
+    path('test',view=views.getRoutes,name="routes"),
+    path('test_post',view=views.test_post_request,name="post request")
 ]
--- Server/myapi/views.py
+++ Server/myapi/views.py
@@ -1,34 +1,48 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 
+84
+
+
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
 from . import calcVar
 from .stockData import get_stock_data
+
+US_STOCK_LIST = ["TSM","GOOGL","TSLA","MSFT","AAPL"]
+number_of_share = [1, 2, 3, 4, 5]
+closing_price = [300,400,500,600,700]
 
 
 def getRoutes(request):
 
-    US_STOCK_LIST = ["TSM","GOOGL","TSLA","MSFT","AAPL"]
-    number_of_share = [1, 2, 3, 4, 5]
-    closing_price = [300,400,500,600,700]
-
     get_stock_data.Get_the_stock_portfolio_historical_data_in_the_given_time(US_STOCK_LIST,100)
-    calcVar.single_stock_parametric_method()
+    # calcVar.single_stock_parametric_method()
 
-    print(f'\n\nnumber of share: {number_of_share}')
-    print(f'closing price: {closing_price}')
-    weight = calcVar.get_weight(number_of_share, closing_price)
-    print(f'weight: {weight}\n')
+    # print(f'\n\nnumber of share: {number_of_share}')
+    # print(f'closing price: {closing_price}')
+    # weight = calcVar.get_weight(number_of_share, closing_price)
+    # print(f'weight: {weight}\n')
 
-    pfl = calcVar.portfolio(US_STOCK_LIST, weight, 501)
-    print(f'For portfolio : {US_STOCK_LIST}')
-    print('Expected Portfolio Return:      ', pfl[0])
-    print('Value at Risk 95th CI    :      ', pfl[1])
-    print('Conditional VaR 95th CI  :      ', pfl[2])
+    # pfl = calcVar.portfolio(US_STOCK_LIST, weight, 501)
 
-    calcVar.portfolio(US_STOCK_LIST, weight, 501)
-    calcVar.portfolio_Monte_Carlo_Simulation(10000,US_STOCK_LIST,weight)
+    # print(f'For portfolio : {US_STOCK_LIST}')
+    # print('Expected Portfolio Return:      ', pfl[0])
+    # print('Value at Risk 95th CI    :      ', pfl[1])
+    # print('Conditional VaR 95th CI  :      ', pfl[2])
+
+    # calcVar.portfolio(US_STOCK_LIST, weight, 501)
+    # calcVar.portfolio_Monte_Carlo_Simulation(10000,US_STOCK_LIST,weight)
 
     return JsonResponse("123",safe=False)
 
-    
+@csrf_exempt
+def test_post_request(request):
+    if request.method == "POST":
+        print("is post request")
+        print(request.POST.get('somekey'))
+
+
+    return JsonResponse("qweqweqwe",safe=False)
 
